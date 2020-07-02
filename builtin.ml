912b81b216f8516841fe72f424b89eae31a0c170
Bicering <103768121+Bicering@users.noreply.github.com>
2020-07-02 20:18:12 +0000

    update 1 files: specs.json

--- builtin.ml
+++ builtin.ml
@@ -100,3 +100,182 @@ let constr_nil =
   { cs_res={ typ_desc=Tconstr(type_constr_list, [arg]); typ_level=generic }
   ; cs_arg=type_unit
   ; cs_tag=Constr_tag_regular(2,0)
+  ; cs_kind=Constr_constant
+  }
+
+let constr_cons =
+  let arg1 = { typ_desc=Tvar(ref Tnolink); typ_level=generic } in
+  let arg2 = { typ_desc=Tconstr(type_constr_list, [arg1]); typ_level=generic } in
+  f "::"
+  { cs_res=arg2
+  ; cs_arg={ typ_desc=Tproduct [arg1; arg2]; typ_level=generic }
+  ; cs_tag=Constr_tag_regular(2,1)
+  ; cs_kind=Constr_superfluous 2
+  }
+
+let constr_none =
+  let arg = { typ_desc=Tvar(ref Tnolink); typ_level=generic } in
+  f "None"
+  { cs_res={ typ_desc=Tconstr(type_constr_option, [arg]); typ_level=generic }
+  ; cs_arg=type_unit
+  ; cs_tag=Constr_tag_regular(2,0)
+  ; cs_kind=Constr_constant
+  }
+
+let constr_some =
+  let arg = { typ_desc=Tvar(ref Tnolink); typ_level=generic } in
+  f "Some"
+  { cs_res={ typ_desc=Tconstr(type_constr_option, [arg]); typ_level=generic }
+  ; cs_arg=arg
+  ; cs_tag=Constr_tag_regular(2,1)
+  ; cs_kind=Constr_regular
+  }
+
+let generic_var = { typ_desc=Tvar (ref Tnolink); typ_level=generic }
+let generic_var2 = { typ_desc=Tvar (ref Tnolink); typ_level=generic }
+
+(* global type_desc *)
+
+let () =
+  let f info =
+    { qualid=info.ty_constr.qualid; info=info }
+  in
+  List.iter add_global_type
+  [ f { ty_constr=type_constr_unit
+      ; ty_arity=0
+      ; ty_desc=Variant_type[constr_void]
+      }
+  ; f { ty_constr=type_constr_bool
+      ; ty_arity=0
+      ; ty_desc=Variant_type[constr_false; constr_true]
+      }
+  ; f { ty_constr=type_constr_char
+      ; ty_arity=0
+      ; ty_desc=Abstract_type
+      }
+  ; f { ty_constr=type_constr_int
+      ; ty_arity=0
+      ; ty_desc=Abstract_type
+      }
+  ; f { ty_constr=type_constr_float
+      ; ty_arity=0
+      ; ty_desc=Abstract_type
+      }
+  ; f { ty_constr=type_constr_exn
+      ; ty_arity=0
+      ; ty_desc=Variant_type[]
+      }
+  ; f { ty_constr=type_constr_string
+      ; ty_arity=0
+      ; ty_desc=Abstract_type
+      }
+  ; f { ty_constr=type_constr_option
+      ; ty_arity=1
+      ; ty_desc=Variant_type[constr_none; constr_some]
+      }
+  ; f { ty_constr=type_constr_list
+      ; ty_arity=1
+      ; ty_desc=Variant_type[constr_nil; constr_cons]
+      }
+  ; f { ty_constr=type_constr_array
+      ; ty_arity=1
+      ; ty_desc=Abstract_type
+      }
+  ];
+  List.iter add_global_constr
+  [ constr_void; constr_false; constr_true; constr_nil; constr_cons
+  ; constr_none; constr_some
+  ; constr_match_failure; constr_division_by_zero
+  ]
+
+let () =
+  let boolop (op,p) =
+    add_global_value
+    { qualid=Lident op
+    ; info={ v_typ=type_arrow type_bool (type_arrow type_bool type_bool)
+          ; v_prim=Prim(2, p)
+          }
+    }
+  in
+  let intop (op,p) =
+    add_global_value
+    { qualid=Lident op
+    ; info={ v_typ=type_arrow type_int (type_arrow type_int type_int)
+          ; v_prim=Prim(2, p)
+          }
+    }
+  in
+  let floatop (op,p) =
+    add_global_value
+    { qualid=Lident op
+    ; info={ v_typ=type_arrow type_float (type_arrow type_float type_float)
+          ; v_prim=Prim(2, p)
+          }
+    }
+  in
+  let polyop (op,p) =
+    add_global_value
+    { qualid=Lident op
+    ; info={ v_typ=gen_type_arrow generic_var (gen_type_arrow generic_var type_bool)
+          ; v_prim=Prim(2, p)
+          }
+    }
+  in
+  List.iter boolop
+  [ "&&", Psequand
+  ; "||", Psequor
+  ];
+  List.iter intop
+  [ "+", Paddint
+  ; "-", Psubint
+  ; "*", Pmulint
+  ; "/", Pdivint
+  ; "mod", Pmodint
+  ; "land", Pandint
+  ; "lor", Porint
+  ; "lxor", Pxorint
+  ; "lsl", Plslint
+  ; "lsr", Plsrint
+  ; "asr", Pasrint
+  ];
+  List.iter floatop
+  [ "+.", Pfloat Paddfloat
+  ; "-.", Pfloat Psubfloat
+  ; "*.", Pfloat Pmulfloat
+  ; "/.", Pfloat Pdivfloat
+  ];
+  List.iter polyop
+  [ "==", Ptest Ptest_eq
+  ; "!=", Ptest Ptest_neq
+  ; "=", Pccall(2, "equal")
+  ; "<>", Pccall(2, "notequal")
+  ; "<", Pccall(2, "less")
+  ; "<=", Pccall(2, "lessequal")
+  ; ">", Pccall(2, "greater")
+  ; ">=", Pccall(2, "greaterequal")
+  ]
+
+(* global value *)
+
+let () =
+  (*let ty = gen_type_array ({ typ_desc=Tvar(ref (Tlink generic_var)); typ_level=notgeneric }) in*)
+  let ty = gen_type_array generic_var in
+  add_global_value
+  { qualid=Ldot(Lident "Array", "get")
+  ; info={ v_typ=gen_type_arrow ty (gen_type_arrow type_int generic_var)
+         ; v_prim=Prim(2, Pgetarrayitem)
+         }
+  };
+  add_global_value
+  { qualid=Ldot(Lident "Array", "make")
+  ; info={ v_typ=gen_type_arrow type_int (gen_type_arrow generic_var ty)
+         ; v_prim=Prim(2, Pmakearray false)
+         }
+  };
+  add_global_value
+  { qualid=Ldot(Lident "Array", "set")
+  ; info={ v_typ=gen_type_arrow ty (gen_type_arrow type_int (gen_type_arrow generic_var type_unit))
+         ; v_prim=Prim(3, Psetarrayitem)
+         }
+  };
+  add_global_value
