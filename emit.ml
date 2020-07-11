93254bb2161ace511b22661c4d1010f18071afd2
Bicering <103768121+Bicering@users.noreply.github.com>
2020-07-11 09:30:15 +0000

    .....

--- emit.ml
+++ emit.ml
@@ -1,4 +1,16 @@
 open Error
 open Exe
 open Lambda
-open Ins
+open Instruction
+open Opcode
+open Syntax
+
+(* code buffer *)
+
+let out_buf = ref (Bytes.create 256)
+let out_pos = ref 0
+
+let o b =
+  let len = Bytes.length !out_buf in
+  if !out_pos >= len then (
+    let 
