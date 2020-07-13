d4fcade639eece1c22c7324fc8e6ff16c804bc34
Bicering <103768121+Bicering@users.noreply.github.com>
2020-07-13 15:55:34 +0000

    asdfadsf

--- emit.ml
+++ emit.ml
@@ -13,4 +13,12 @@ let out_pos = ref 0
 let o b =
   let len = Bytes.length !out_buf in
   if !out_pos >= len then (
-    let 
+    let newbuf = Bytes.create (len*2) in
+    Bytes.blit !out_buf 0 newbuf 0 len;
+    out_buf := newbuf
+  );
+  Bytes.set !out_buf !out_pos (b land 255 |> char_of_int);
+  incr out_pos
+
+let oo w =
+  if w < -32768 || 32767 < 
