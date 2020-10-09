43488d8ebdb1d94b444b0268ab8e42d80e5619ad
Bicering <103768121+Bicering@users.noreply.github.com>
2020-10-09 10:25:00 +0000

    TODO: update.

--- fpretty.ml
+++ fpretty.ml
@@ -10,4 +10,13 @@ module List = struct
     match n, xs with
     | 0, _ | _, [] -> []
     | n, x::xs -> x :: take (n-1) xs
-  let rec dro
+  let rec drop n xs =
+    match n, xs with
+    | 0, _ | _, [] -> xs
+    | n, _::xs -> drop (n-1) xs
+  let rec last = function
+    | [] -> failwith "last: empty list"
+    | [x] -> x
+    | x::xs -> last xs
+  let split_at k xs =
+  
