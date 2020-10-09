4a1cdc97c132f79b1e13dc3d633a2f1036508a00
Bicering <103768121+Bicering@users.noreply.github.com>
2020-10-09 06:06:26 +0000

    [cs] add folders to scan

--- fpretty.ml
+++ fpretty.ml
@@ -2,4 +2,12 @@ module List = struct
   include List
   let zip xs ys =
     let rec go acc = function
-      | [], _ | _, [
+      | [], _ | _, [] -> List.rev acc
+      | x::xs, y::ys -> go ((x,y)::acc) (xs,ys)
+    in
+    go [] (xs,ys)
+  let rec take n xs =
+    match n, xs with
+    | 0, _ | _, [] -> []
+    | n, x::xs -> x :: take (n-1) xs
+  let rec dro
