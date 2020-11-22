3da1d8765917bad76f9f409d8422a6c729b80428
Bicering <103768121+Bicering@users.noreply.github.com>
2020-11-22 09:32:02 +0000

    error page

--- front.ml
+++ front.ml
@@ -169,3 +169,202 @@ let divide_constr_matching (rows,paths) =
               | _ -> arg::ps
           end in
         let constrs, others = go rest in
+        add_to_division (make_constr_match cd paths) constrs cd.info.cs_tag (ps,act),
+        others
+    | rows ->
+        [], (rows, paths)
+  in
+  go (simplify_upper_left rows)
+
+let upper_left_pattern =
+  let rec go p =
+    match p.p_desc with
+    | Ppat_alias(p,_)
+    | Ppat_constraint(p,_)
+    | Ppat_or(p,_) -> go p
+    | _ -> p
+  in
+  go
+
+let get_span_of_constr cd =
+  match cd.info.cs_tag with
+  | Constr_tag_extensible _ -> 0
+  | Constr_tag_regular(n,_) -> n
+
+let rec conquer_matching =
+  let rec conquer_divided_matching = function
+    | [] ->
+        [], Total
+    | (key,ms)::rest ->
+        let lambda1, total1 = conquer_matching !ms
+        and list2, total2 = conquer_divided_matching rest in
+        (key,lambda1)::list2,
+        (match total1,total2 with
+        | Total,Total -> Total
+        | Partial,_
+        | _,Partial -> Partial
+        | _ -> Dubious)
+  in
+  function
+  | [], _ ->
+      Lstaticraise, Partial
+  | ([],act)::rest, _ ->
+      act, Total
+  | (ul::_,_)::_, (path::_) as mat ->
+      let ul = upper_left_pattern ul in
+      begin match ul.p_desc with
+      | Ppat_any
+      | Ppat_var _ ->
+          let vars, rest = split_matching mat in
+          let lambda1, total1 = conquer_matching vars
+          and lambda2, total2 = conquer_matching rest in
+          if total1 = Total then
+            lambda1, Total
+          else
+            Lstaticcatch(lambda1, lambda2),
+            (if total2 = Total then Total else Dubious)
+      | Ppat_constant _ ->
+          let constants, others = divide_constant_matching mat in
+          let divs1, _ = conquer_divided_matching constants
+          and lambda2, total2 = conquer_matching others in
+          Lstaticcatch(Lcond(path, divs1), lambda2), total2
+      | Ppat_constr _ ->
+          let constrs, others = divide_constr_matching mat in
+          let divs, total1 = conquer_divided_matching constrs
+          and lambda, total2 = conquer_matching others in
+          let ndivs = List.length divs
+          and span =
+            match ul.p_desc with
+            | Ppat_constr(id,_) ->
+                let cd = find_constr_desc id in
+                get_span_of_constr cd
+            | _ -> assert false
+          in
+          if span = ndivs && total1 = Total then
+            Lswitch(span, path, divs), Total
+          else
+            Lstaticcatch(Lswitch(span, path, divs), lambda), total2
+      | Ppat_array p ->
+          let arity = List.length p in
+          conquer_matching @@ divide_tuple_matching arity mat
+      | Ppat_tuple p ->
+          let arity = List.length p in
+          conquer_matching @@ divide_tuple_matching arity mat
+      | _ -> assert false
+      end
+  | _ -> assert false
+
+let partial_fun (l,m) =
+  Lprim(Praise, [Lprim(Pmakeblock match_failure_tag, [
+    Lprim(Pmakeblock(Constr_tag_regular(1,0)),
+    [ Lconst(Const_base(Const_string "//toplevel//"))
+    ; Lconst(Const_base(Const_int l))
+    ; Lconst(Const_base(Const_int m))
+    ])
+  ])])
+
+let translate_matching fail env rows =
+  let rec go = function
+    | 0 -> []
+    | n -> Lvar(n-1) :: go (n-1)
+  in
+  let row_len = List.hd rows |> fst |> List.length in
+  let lambda, total = conquer_matching (rows, go row_len) in
+  match total with
+  | Total -> lambda
+  | _ -> Lstaticcatch(lambda, fail) (* FIXME report error *)
+
+(* toplevel expression *)
+
+let rec transl_expr env expr =
+  let rec go expr =
+    match expr.e_desc with
+    | Pexpr_apply(e,es) ->
+        Lapply(go e, List.map go es)
+    | Pexpr_array es ->
+        Lprim(Pmakearray true, Lconst (Const_base (Const_int (List.length es)))::List.map go es)
+    | Pexpr_constant c ->
+        Lconst(Const_base c)
+    | Pexpr_constr(id,arg) ->
+        let cd = find_constr_desc id in
+        begin match arg with
+        | None ->
+            begin match cd.info.cs_kind with
+            | Constr_constant ->
+                Lprim(Pmakeblock cd.info.cs_tag, [])
+            | _ ->
+                Labstract(Lprim(Pmakeblock cd.info.cs_tag, [Lvar 0]))
+            end
+        | Some arg ->
+            Lprim(Pmakeblock cd.info.cs_tag, [go arg])
+        end
+    | Pexpr_constraint(e,_) ->
+        go e
+    | Pexpr_for(name,start,stop,up,body) ->
+        let env' = [name,[]]::[]::[]::env in
+        Lfor(go start, transl_expr ([]::env) stop, up,
+        transl_expr env' body)
+    | Pexpr_function pes ->
+        Labstract(transl_match_check expr.e_loc env @@
+          List.map (fun (p,e) -> [p],e) pes)
+    | Pexpr_ident id ->
+        begin try
+          match id with
+          | Ldot _ ->
+              raise Not_found
+          | Lident name ->
+              transl_access env name
+        with Not_found ->
+          let vd = find_value_desc id in
+          match vd.info.v_prim with
+          | Not_prim ->
+              Lprim(Pgetglobal id, []) (* TODO *)
+          | Prim(arity,prim) ->
+              let rec go args n =
+                if n >= arity then
+                  Lprim(prim, args)
+                else
+                  Labstract (go (Lvar n::args) (n+1))
+              in
+              go [] 0
+        end
+    | Pexpr_if(cond,ifso,ifnot) ->
+        begin match ifnot with
+        | None ->
+            Lif(go cond, go ifso, Lconst(Const_block 0))
+        | Some ifnot ->
+            Lif(go cond, go ifso, go ifnot)
+        end
+    | Pexpr_let(isrec,binds,body) ->
+        if isrec then (
+          let ps, acts = List.split binds in
+          let env' = make_env env ps in
+          Lletrec(List.map (transl_expr env') acts,
+          transl_match_check expr.e_loc env [ps,body])
+        ) else
+          Llet(transl_bind env binds,
+          transl_match_check expr.e_loc env [List.map fst binds,body])
+    | Pexpr_sequence(e1,e2) ->
+        Lsequence(go e1, go e2)
+    | Pexpr_try(body,pes) ->
+        Lcatch(go body,
+          transl_match env @@ List.map (fun (p,e) -> [p],e) pes)
+    | Pexpr_tuple es ->
+        Lprim(Pmakeblock(Constr_tag_regular(1,0)), List.map go es)
+  in
+  go expr
+
+and transl_bind env = function
+  | [] -> []
+  | (_,e)::pes ->
+      transl_expr env e :: transl_bind ([]::env) pes
+
+and transl_match env psas =
+  let rows = List.map (fun (ps,act) ->
+    ps, transl_expr (make_env env ps) act) psas in
+  translate_matching (Lprim(Praise, [Lvar 0])) env rows
+
+and transl_match_check loc env psas =
+  let rows = List.map (fun (ps,act) ->
+    ps, transl_expr (make_env env ps) act) psas in
+  translate_matching (partial_fun loc) env rows
