0d4356aef2eb21587770098582f6d54cc546ef5b
Bicering <103768121+Bicering@users.noreply.github.com>
2020-11-24 14:50:04 +0000

    Update latest.html

--- global.ml
+++ global.ml
@@ -3,4 +3,10 @@ open Syntax
 let cur_module = ref "Main"
 
 let all_constrs : (long_ident, constr_desc global) Hashtbl.t = Hashtbl.create 17
-let all_types : (long_ident, type_de
+let all_types : (long_ident, type_desc global) Hashtbl.t= Hashtbl.create 17
+let all_values : (long_ident, value_desc global) Hashtbl.t= Hashtbl.create 17
+
+let find_desc sel name =
+  Hashtbl.find sel name
+
+let find_constr_de
