ca1b740b648c014873b3f9b1637502a0291dc2a9
Bicering <103768121+Bicering@users.noreply.github.com>
2020-12-24 12:12:03 +0000

    * NOT RELEASED YET

--- prettiest.ml
+++ prettiest.ml
@@ -1,1 +1,11 @@
-let (
+let (%) f g x = f (g x)
+
+type ('a,'b) either = Left of 'a | Right of 'b
+
+let partition_eithers xs =
+  let rec go ls rs = function
+    | [] ->
+        List.rev ls, List.rev rs
+    | x::xs ->
+        match x with
+        | Le
