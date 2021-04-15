9ee1b9bef3247374398e1533f339d8bfec54a46c
Bicering <103768121+Bicering@users.noreply.github.com>
2021-04-15 06:48:42 +0000

    change in readme

--- runtime/compare.c
+++ runtime/compare.c
@@ -3,4 +3,11 @@
 #include "str.h"
 
 // comparison on nonrecursive structures
-intptr_t cmp_value(value v1, valu
+intptr_t cmp_value(value v1, value v2)
+{
+  if (v1 == v2) return 0;
+  if (Is_int(v1) || Is_int(v2)) return Int_val(v1)-Int_val(v2);
+  u8 t1 = Tag_val(v1), t2 = Tag_val(v2);
+  if (t1 != t2) return (intptr_t)t1-t2;
+  switch (t1) {
+  case String_t
