025d17f357b1e93369858eaa64561892f97d63ea
Bicering <103768121+Bicering@users.noreply.github.com>
2021-04-24 13:18:55 +0000

    more doc changes

--- runtime/error.c
+++ runtime/error.c
@@ -4,4 +4,18 @@
 #include "error.h"
 #include "value.h"
 
-void raise_with_string(u8 tag, const 
+void raise_with_string(u8 tag, const char *msg)
+{
+  fprintf(stderr, "%s\n", msg);
+  abort();
+}
+
+void not_implemented(const char *msg)
+{
+  fprintf(stderr, "%s\n", msg);
+  abort();
+}
+
+void invalid_argument(const char *msg)
+{
+  ra
