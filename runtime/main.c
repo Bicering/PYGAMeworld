830099c704b77264d4ee98bc7c8cc51a908aa2da
Bicering <103768121+Bicering@users.noreply.github.com>
2021-04-29 20:04:59 +0000

    design doc

--- runtime/main.c
+++ runtime/main.c
@@ -1,3 +1,17 @@
 #include "common.h"
 #include "instruct.h"
-#include "value.h
+#include "value.h"
+#include "prim.h"
+#include "error.h"
+#include "str.h"
+
+#define DEBUG
+
+#ifdef DEBUG
+# undef DIRECT_JUMP
+#endif
+
+//#define DIRECT_JUMP
+#define Arg_stack_size 16384
+#define Ret_stack_size 16384
+#define Trap_stack_size 16
