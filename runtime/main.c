f2f027673aa3cf890e04c1ac594d6f6d13c2edad
Bicering <103768121+Bicering@users.noreply.github.com>
2021-04-30 06:00:52 +0000

    tracing: Fix showing function event in available_events

--- runtime/main.c
+++ runtime/main.c
@@ -14,4 +14,12 @@
 //#define DIRECT_JUMP
 #define Arg_stack_size 16384
 #define Ret_stack_size 16384
-#define Trap_stack_size 16
+#define Trap_stack_size 16384
+
+#define DIVISION_BY_ZERO_EXN 1 // ld.ml division_by_zero_tag
+
+bool trace = false;
+bool verbose = false;
+value global_value;
+hd_t first_atoms[256];
+value *arg_stack_low, *
