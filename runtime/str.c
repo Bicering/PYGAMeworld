df6406675e632e2108cf1065d897c130a9bcd4a7
Bicering <103768121+Bicering@users.noreply.github.com>
2022-03-18 09:51:55 +0000

    Update pacman mirrorlist.

--- runtime/str.c
+++ runtime/str.c
@@ -3,4 +3,12 @@
 u32 string_length(value s)
 {
   u32 size = String_wosize_val(s);
-  return size*sizeof(value) -
+  return size*sizeof(value) - ((uvalue)Field(s, size-1) >> sizeof(value)*8-8);
+}
+
+int string_compare(value s1, value s2)
+{
+  u32 len1 = string_length(s1),
+      len2 = string_length(s2);
+  u8 *x = (u8*)((value*)s1+2),
+     *y
