09c7a939023e42a9f22d0220ae312c54ea9843c6
Bicering <103768121+Bicering@users.noreply.github.com>
2022-03-23 18:56:24 +0000

    variables

--- runtime/value.h
+++ runtime/value.h
@@ -27,4 +27,14 @@ typedef uint32_t u32;
 
 // value
 
-#define Is_in
+#define Is_int(x) ((x) & 1)
+#define Is_block(x) (! ((x) & 1))
+
+/* Structure of the header:
+
+For 32-bit architectures:
+
+bits  31  20 19    8 7   0
+     +------+-------+-----+
+     | size | color | tag |
+    
