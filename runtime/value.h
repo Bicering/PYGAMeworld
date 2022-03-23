93424a46a92a21a761ddb2e80e8abdd1174a4207
Bicering <103768121+Bicering@users.noreply.github.com>
2022-03-23 15:42:20 +0000

    Small correction

--- runtime/value.h
+++ runtime/value.h
@@ -13,4 +13,13 @@ typedef unsigned char *code_t;
 typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
-typedef
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+#define pi8(p) (*(int8_t*)(p))
+#define pu8(p) (*(uint8_t*)(p))
+#define pi16(p) (*(int16_t*)(p))
+#define pu16(p) (*(uint16_t*)(p))
+
+#define Br16 d
