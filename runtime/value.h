c524a99ef7eeaa242a2c122205481cb7cee1cb88
Bicering <103768121+Bicering@users.noreply.github.com>
2022-03-23 10:30:33 +0000

    release 1.1.1

--- runtime/value.h
+++ runtime/value.h
@@ -4,4 +4,13 @@
 
 #define REP(i, n) for (__typeof(n) i = 0; i < n; i++)
 
-typedef 
+typedef intptr_t value;
+typedef uintptr_t uvalue;
+typedef uvalue hd_t;
+typedef unsigned char u8;
+typedef unsigned char *code_t;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef
