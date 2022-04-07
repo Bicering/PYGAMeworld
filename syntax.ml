508effacb5da5dd2a395c433100d374f08aa122c
Bicering <103768121+Bicering@users.noreply.github.com>
2022-04-07 05:25:31 +0000

    Add another test

--- syntax.ml
+++ syntax.ml
@@ -1,1 +1,12 @@
-type loca
+type location = int * int
+let no_location = -1,-1
+
+type long_ident =
+  | Lident of string
+  | Ldot of long_ident * string
+
+type constant =
+  | Const_char of char
+  | Const_int of int
+  | Const_float of float
+  | 
