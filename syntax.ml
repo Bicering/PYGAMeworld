b5ffc9f93cb43258bad43fdbc29edbac3ce3da64
Bicering <103768121+Bicering@users.noreply.github.com>
2022-04-07 09:39:24 +0000

    merge 70 to 71

--- syntax.ml
+++ syntax.ml
@@ -9,4 +9,15 @@ type constant =
   | Const_char of char
   | Const_int of int
   | Const_float of float
-  | 
+  | Const_string of string
+
+(* tag *)
+
+type constr_tag =
+  | Constr_tag_regular of int * int
+  | Constr_tag_extensible of long_ident * int
+
+let new_exttag_stamp =
+  let stamp = ref 0 in
+  fun () ->
+    l
