4923bf51c0abd46596b42ef96bcff47648dbe47c
Bicering <103768121+Bicering@users.noreply.github.com>
2023-02-18 06:31:21 +0000

    Update mini file

--- tests/levenshtein.ml
+++ tests/levenshtein.ml
@@ -1,4 +1,11 @@
 let min a b =
   if a < b then a else b
 
-let lev
+let levenshtein a b =
+  let m = String.length a
+  and n = String.length b in
+  let s = Array.make 2 [||] in
+  s.(0) <- Array.make (n+1) 0;
+  s.(1) <- Array.make (n+1) 0;
+  for j = 1 to n do
+    s.
